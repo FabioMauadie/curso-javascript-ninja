e6898050221a9ab31879f9e20ee637bda2d796d1
Fábio Mauadié <38436258+FabioMauadie@users.noreply.github.com>
2019-06-14 19:43:40 -0300

    projetoCalculadora.js

--- challenge-23/challenge-23.js
+++ challenge-23/challenge-23.js
@@ -1,3 +1,4 @@
+
 /*
 Vamos desenvolver mais um projeto. A ideia é fazer uma mini-calculadora.
 As regras são:
@@ -23,3 +24,141 @@ multiplicação (x), então no input deve aparecer "1+2x".
 input;
 - Ao pressionar o botão "CE", o input deve ficar zerado.
 */
+  <!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>index-23</title>
+</head>
+<body>
+    <h1 style="color: red" align="center">calculadora</h1>
+    <br><br>
+    <!--esse readonly desativa o campo deixando apenas como visor nao podendo escrever-->
+    <input type="text" value="0" readonly data-js="visor">
+    <br><br>
+    <div class=" buttons-numbers">
+    <button data-js="button-number" value="0">0</button>
+    <button data-js="button-number" value="1">1</button>
+    <button data-js="button-number" value="2">2</button>
+    <button data-js="button-number" value="3">3</button>
+    <button data-js="button-number" value="4">4</button>
+    <button data-js="button-number" value="5">5</button>
+    <button data-js="button-number" value="6">6</button>
+    <button data-js="button-number" value="7">7</button>
+    <button data-js="button-number" value="8">8</button>
+    <button data-js="button-number" value="9">9</button>
+</div>
+<br><br>
+<div class="operation">
+<button data-js="button-operation" data-id="button-operation-somar" value="+">+</button>
+<button data-js="button-operation" data-id="button-operation-subtrair"value="-">-</button>
+<button data-js="button-operation" data-id="button-operation-multiplicar"value="x">x</button>
+<button data-js="button-operation" data-id="button-operation-dividir"value="÷">÷</button>
+<button data-js="button-resultado" data-id="button-operation-resultado" value="=">=</button>
+<button data-js="button-limpar" data-id="button-operation-limpar" value="CE">CE</button>
+</div>
+<script src="js/calculadora.js"></script>
+    <script src="js/challenge-23.js"></script>
+</body>
+</html>
+  
+  ..............................JAVASCRIPT......................................................
+ (function(win,doc){
+ 'user strict';
+          //vai pegar input visor do html
+        var $visor = doc.querySelector('[data-js="visor"]'); 
+        //vai pegar todos os numeros de 0 a 9 do html
+        var $buttonsNumbers = doc.querySelectorAll('[data-js="button-number"]');
+        // vai pegar o operador CE reponsavel por limpar a tela 
+        var $buttonCE = doc.querySelector('[data-id="button-operation-limpar"]');
+
+        var $operacoes = doc.querySelectorAll('[data-js="button-operation"]');
+
+        var $buttonIgual = doc.querySelector('[data-js="button-resultado"]');
+
+        //////////////////////////////////////////////////////////////////////////////////////
+        //vai adicionar um evento de click nos botoes e mostrar ele no visor
+        //vai pecorrer todos os botoes de 0 a 9 e vai dar um evento de click e chamar a funcao mostra na tela 
+        Array.prototype.forEach.call($buttonsNumbers,function(button){
+
+        //vai dar um evento de click e vai chamar a funcao mostrar no visor
+            button.addEventListener('click',mostrarNoVisor,false);
+        });
+
+        //vai pecorrer todos os botoes de operaçoes e vai dar um evento de click e chamar a funcao operacoesNoVisor 
+        Array.prototype.forEach.call($operacoes,function(button){
+            button.addEventListener('click',OperacoesNoVisor,false);
+        });
+        ////////////////////////////////////////////////////////////////////////////////////////
+
+
+        //vai dar um evento de click no botao CE e vai chamar a funcao limparVisor
+        $buttonCE.addEventListener('click',limparVisor,false);
+        ///////////////////////////////////////////////////////////////////////////////////////
+
+            //funcao que faz mostrar os numeros clicados no visor
+            function mostrarNoVisor(event){
+               $visor.value += this.value; 
+               /* visor.value vai pegar tudo que tem no visor e vai concatenar com  this.value que representa o botao que está sendo clicado e vai pegar o valor*/
+            }
+            //////////////////////////////////////////////////////////////////////////////////
+            //funcao faz com que só seja permitido aparecer apenas 1 operador no visor 
+            function OperacoesNoVisor(){
+                $visor.value = removeUltimoItem($visor.value);//vai remover o operador nao deixando colocar 2
+
+                $visor.value += this.value;
+
+            }
+
+            function removeUltimoItem(number){
+                if(verificarUltimoDigito(number)) {//vai verificar se o ultimo digito é alguma operacao e passa as operacoes como parametro
+
+                    //caso o ultimo digito seja operador vai pegar o visor e retirar o ultimo item com slice
+                    return number.slice(0,-1);
+                    }
+                    return number;
+            }
+            //funcao que faz a verificação do ultimo digito
+            function verificarUltimoDigito(number){
+                var operation = ['+','-','x','÷'];//se for alguns desses simbolos nao pode aparecer outro seguido
+                var lastItem = number.split('').pop();
+                 return operation.some(function(operator){//some passa por todos os item do array e verificar se tem pelo menos 1 deles
+                    return operator === lastItem;
+                 })
+
+            }
+            ///////////////////////////////////////////////////////////////////////////////////
+            //funcao que vai ser chamada quando clicar no botao CE
+            function limparVisor(){
+                $visor.value = 0; //vai pegar o visor e colocar o valor 0
+            };
+           ////////////////////////////////////////////////////////////////////////////////////
+            //vai dar um evento de click no botao = e vai chamar a funcao limparVisor
+        $buttonIgual.addEventListener('click',resultadoNoVisor,false);
+
+
+        function resultadoNoVisor(){
+            $visor.value= removeUltimoItem($visor.value);//está chamando a funcao q elimina o operador caso ele seja o ultimo digito antes de dar o resultado
+            var todosValores = $visor.value.match(/\d+[+-x÷]?/g);//vai pegar todos os valores seguido de um operador
+                $visor.value = todosValores.reduce(function(accumulated,atual){
+                var primeiroValor = accumulated.slice(0,-1);
+                var operador = accumulated.split('').pop();
+                var segundoValor = removeUltimoItem(atual);
+                var proximOperador = verificarUltimoDigito(atual) ? atual.split('').pop():'';
+                switch(operador){
+                    case('+'):
+                    return (Number(primeiroValor)  + Number(segundoValor)) + proximOperador;
+                    case('-'):
+                    return(Number(primeiroValor)  - Number(segundoValor)) + proximOperador;
+                    case ('x'):
+                        return (Number(primeiroValor)  * Number(segundoValor)) + proximOperador;
+                    case ('÷'):
+                        return (Number(primeiroValor)  / Number(segundoValor)) + proximOperador;
+                }
+            });
+
+
+        }
+})(window,document);
