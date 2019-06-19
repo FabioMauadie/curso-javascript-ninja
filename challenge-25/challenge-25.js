d6b903bacc77bb6ce8e3125142d39f31058b870a
Fábio Mauadié <38436258+FabioMauadie@users.noreply.github.com>
2019-06-18 21:47:02 -0300

    testando eventos

--- challenge-25/challenge-25.js
+++ challenge-25/challenge-25.js
@@ -1,3 +1,4 @@
+(function(){
 /*
 Essa semana você terá dois desafios:
 1) Revisar todo o contéudo passado até aqui, e ver se você realmente entendeu
@@ -14,3 +15,72 @@ https://developer.mozilla.org/en-US/docs/Web/Events#Categories
 Tente aplicar na prática alguns dos eventos que estão ali e coloque nesse
 desafio os experimentos legais que você conseguir desenvolver :D
 */
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>index-25</title>
+</head>
+<body>
+<br>
+<div align="center">
+<button  data-js="mouse"><b>passe o mouse</b></button>
+<br>
+<br>
+<br>
+<button  data-js="botao">click</button>
+<br>
+<br>
+<br>
+<button data-js="cliqueDuasVezes">click 2 vezes</button>
+<br>
+<br>
+<br>
+<input data-js="text" type="text">
+<select data-js="select">
+<option value="1">1</option>
+<option value="2">2</option>
+<option value="3">3</option>
+</select>
+<br>
+<br>
+<br>
+
+</div>
+    <script src="js/challenge-25.js"></script>
+</body>
+</html>
+  
+ 
+  
+  var $mouse = document.querySelector('[data-js="mouse"]');
+$mouse.addEventListener('mousemove',passeMouse,false);
+function passeMouse(event){
+    event.preventDefault();
+    alert(" o mouse foi passado pelo titulo :)");
+}
+
+var $cliquei = document.querySelector('[data-js="botao"]');
+$cliquei.addEventListener('click',click,false);
+function click(event){
+    event.preventDefault();
+    alert(" cliquei :)");
+}
+
+var $cliqueVezes = document.querySelector('[data-js="cliqueDuasVezes"]');
+$cliqueVezes.addEventListener('dblclick',dblclique,false);
+function dblclique(event){
+    event.preventDefault();
+    alert(" foi clicado 2 vezes!!!)");
+}
+var $inputText = document.querySelector('[data-js="select"]');
+$inputText.addEventListener('change',change,false);
+function change(event){
+    event.preventDefault();
+   document.querySelector(['[data-js="text"]']).value = this.value; 
+
+}
+  
+  })();
